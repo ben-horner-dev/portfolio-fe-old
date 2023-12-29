fecda8a69e9e275c04246a9c4d2fec558106b27a
Ben Horner <info@benhorner.dev>
2023-12-29 17:54:52 +0000

    feat: Add pre-commit linting & formatting (#13)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import './app.css'
 import App from './App.svelte'
 
+const targetElement = document.getElementById('app')
+
+if (!targetElement) {
+  throw new Error("Couldn't get the 'app' element")
+}
 const app = new App({
-  target: document.getElementById('app'),
+  target: targetElement,
 })
 
 export default app
