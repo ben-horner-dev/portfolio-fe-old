e2447d6bdd3303ec0eea3d60738d1c602054ae60
GitHub Ben Horner Dev <info@benhorner.dev>
2023-12-31 15:27:06 +0000

    chore: initial fe setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      organisms: path.resolve('./src/lib/organisms'),
+      atoms: path.resolve('./src/lib/atoms'),
+      molecules: path.resolve('./src/lib/molecules'),
+      pages: path.resolve('./src/lib/pages')
+    }
+  }
 })
