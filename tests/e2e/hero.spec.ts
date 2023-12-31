3e466fb368746cd0f5abad77c9f40ae9913b2dc1
GitHub Ben Horner Dev <info@benhorner.dev>
2023-12-31 15:50:22 +0000

    tests: Add hero tests

--- tests/e2e/hero.spec.ts
+++ tests/e2e/hero.spec.ts
@@ -4,12 +4,36 @@ test.beforeEach(async ({ page }) => {
   await page.goto('localhost:5173')
 })
 
-test.describe('h1 text', () => {
+test.describe('Hero Page', () => {
+  test('h1 text should appear', async ({ page }) => {
+    const text = await page.innerText('h1')
+    expect(text).toBe("Hello, I'm Ben:")
+  })
+
+  test('long form div text should appear', async ({ page }) => {
+    await page.setViewportSize({ width: 816, height: 1080 })
+    const text = await page.innerText('.full-screen-text')
+
+    expect(text).toBe(
+      `I am a full stack developer with a mission to create elegant apps that are reliable, maintainable and scalable. My stack of choice is Flask, FastApi or Django for the backend, React or Svelte plus Typescript for the frontend with MySql, Postgres or MongoDB for the database but I am always learning new technologies. Please feel free to explore my previous work and contact me if you have any questions:`
+    )
+  })
+
+  test('Short form div text should appear', async ({ page }) => {
+    await page.setViewportSize({ width: 814, height: 1080 })
+    const text = await page.innerText('.mobile-screen-text')
+
+    expect(text).toBe(
+      `I am a full stack developer with a mission to create elegant apps that are reliable, maintainable and scalable.`
+    )
+  })
+
   test('text should appear', async ({ page }) => {
     const text = await page.innerText('h1')
     expect(text).toBe("Hello, I'm Ben:")
   })
-  test('Hero btn', async ({ page }) => {
+
+  test('Contact btn', async ({ page }) => {
     // Go to the page where the Hero component is rendered
 
     // Get the button element
